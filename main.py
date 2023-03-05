2d1b9ea08cdf0c4f1e33d468a2ef27fd1318d91e
MichalSlusarski <m.slusarski@student.uw.edu.pl>
2023-03-05 18:10:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,17 @@ def index():
 def ideaboard():
     return render_template('ideaboard.html')
 
+@app.route('/ideaboard-guest')
+def ideaboard_guest():
+    return render_template('ideaboard_guest.html')
+
+@app.route('/add')
+def add():
+    return render_template('add_idea.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port = 8080, debug = True)
