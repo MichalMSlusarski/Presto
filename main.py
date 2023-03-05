d11421f6ec4391661a2145cf029ea41bda5a85ba
MichalSlusarski <m.slusarski@student.uw.edu.pl>
2023-03-05 16:52:08 +0100

    Update main

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/ideaboard')
+def ideaboard():
+    return render_template('ideaboard.html')
+
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port = 8080, debug = True)
