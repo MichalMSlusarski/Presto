f70943c9dcbe13c2e51f34cbc7ffd587d84fb216
MichalSlusarski <m.slusarski@student.uw.edu.pl>
2023-03-06 23:39:52 +0100

    Trying to move button up

--- templates/script.js
+++ templates/script.js
@@ -1,12 +1,12 @@
 function copyToClipboard() {
-    var copyText = document.getElementById("session-link");
+    var copyText = document.getElementById("copy-link");
   
     copyText.select();
     copyText.setSelectionRange(0, 99999);
   
     navigator.clipboard.writeText(copyText.value);
   
-    alert("Copied the text: " + copyText.value);
+    //alert("Copied the text: " + copyText.value);
 
     var tooltip = document.getElementById("copy-button");
 
