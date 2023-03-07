f61101fce22ce27d5ab0d8f8c9dcc2625b2019ac
MichalSlusarski <m.slusarski@student.uw.edu.pl>
2023-03-07 19:43:58 +0100

    Js updates

--- templates/script.js
+++ templates/script.js
@@ -5,10 +5,17 @@ function copyToClipboard() {
     copyText.setSelectionRange(0, 99999);
   
     navigator.clipboard.writeText(copyText.value);
-  
-    //alert("Copied the text: " + copyText.value);
 
     var tooltip = document.getElementById("copy-button");
 
     tooltip.innerText = "Copied! ✅"
-  } 
+}
+
+function addIdea() {
+    var elemDiv = document.createElement('div');
+    var mainTwo = document.getElementById("mainTwo");
+
+    elemDiv.style.cssText = "opacity: 1; position: relative; background: rgb(245, 245, 245); width: 400px; height: 100px; margin: 10px auto 20px auto; border-radius: 20px; box-shadow: 0 4px 11px rgb(0 0 0 / 0.2); display: flex; flex-direction: column; justify-content: center; align-items: center; font-family: 'Poppins', sans-serif;"
+
+    document.body.mainTwo.appendChild(elemDiv);
+}
