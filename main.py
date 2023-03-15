279233b8bcc78a907d580da6795552ddd44aa05e
MichalSlusarski <m.slusarski@student.uw.edu.pl>
2023-03-15 22:22:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ class user(db.Model):
     email = db.Column(db.String(120), unique = True, nullable = False)
     image_file = db.Column(db.String(20), nullable = False, default = 'rabbit.jpg')
 
+    def __repr__(self):
+        return f'user('{self.email}','{self.image_file}')
+
 idea = {
     'author': 'anonymous1',
     'premise': 'I think we should ',
