6773ef835f2f5cf4d2f7ed5b2121debe028db9d6
MichalSlusarski <m.slusarski@student.uw.edu.pl>
2023-03-16 00:16:15 +0100

    Many

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
 
-app.config['SECRET KEY'] = '455638289da72dc40b6d536f3b26ed6d1865431d07dcf796295087d3c335c6af'
+#app.config['SECRET KEY'] = '455638289da72dc40b6d536f3b26ed6d1865431d07dcf796295087d3c335c6af'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
@@ -21,7 +22,7 @@ class User(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(120), unique = False, nullable = False)
-    date_posted = db.Colum(db.DateTime, nullable = False, default = datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
     content = db.Column(db.Text(240), unique = False, nullable = False)
     additional_img = db.Column(db.String(20), nullable = True)
     label = db.Column(db.String(12), unique = False, nullable = False, default = 'Defualt')
