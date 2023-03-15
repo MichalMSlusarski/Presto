dd0756c420b16096860cf42e02f5d1d6988210f1
MichalSlusarski <m.slusarski@student.uw.edu.pl>
2023-03-15 22:43:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class user(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(120), unique = True, nullable = False)
     image_file = db.Column(db.String(20), nullable = False, default = 'rabbit.jpg')
+    posts = db.relationship('post', backref = 'author', lazy = True)
 
     def __repr__(self):
         return f"user('{self.email}','{self.image_file}')"
