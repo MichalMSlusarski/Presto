55898f4940f80927f40dafe3f62144075686b094
MichalSlusarski <m.slusarski@student.uw.edu.pl>
2023-03-15 22:11:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#app.config['SECRET KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 idea = {
     'author': 'anonymous1',
